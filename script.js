788a94ae405f59563e894d4660bf8baf9107bf6b
nashraf88 <nashraf4998@panther.chaffey.edu>
2023-03-06 00:09:14 -0800

    Hands On Pro 8-1

---
+++ script.js
@@ -0,0 +1,52 @@
+/*
+      JavaScript 6th Edition
+      Chapter 7
+      Hands-on Project 8-1
+
+      Author: Nazia Ashraf
+      Date: 3/5/2023
+
+      Filename: script.js
+   */
+  "use strict";
+  var newAccountArray = [];
+  function createID() {
+    var fname = document.getElementById("fnameinput");
+    var lname = document.getElementById("lnameinput");
+    var zip = document.getElementById("zipinput");
+    var account = document.getElementById("accountidbox");
+    var fields = document.getElementsByTagName("input");
+    var acctid;
+    var firstInit;
+    var lastInit;
+    if((fname !== "") && (lname !== "") && (zip !== "")) {
+        firstInit = fname.value.charAt(0).toUpperCase();
+        lastInit = lname.value.charAt(0).toUpperCase();
+        acctid = firstInit + lastInit + zip.value;
+        account.value = acctid;
+        newAccountArray = [];
+        for (var i = 0; i < fields.length - 1; i++) {
+            newAccountArray.push(fields[i].value);
+        }
+    }
+  }
+
+  function createEventListeners() {
+    var fname = document.getElementById("fnameinput");
+    var lname = document.getElementById("lnameinput");
+    var zip = document.getElementById("zipinput");
+    if (fname.addEventListener) {
+        fname.addEventListener("change", createID, false);
+        lname.addEventListener("change", createID, false);
+        zip.addEventListener("change", createID, false);
+    } else if (fname.attachEvent) {
+        fname.attachEvent("onchange", createID);
+        lname.attachEvent("onchange", createID);
+        zip.attachEvent("onchange", createID);
+    }
+  }
+  if (window.addEventListener) {
+    window.addEventListener("load", createEventListeners, false);
+  } else if (window.attachEvent) {
+    window.attachEvent("onload", createEventListeners);
+  }
